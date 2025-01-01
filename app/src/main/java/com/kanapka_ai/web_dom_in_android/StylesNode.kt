e7150a2560dc9fa4c20141dbcd1f89f1cde0fb92
FylypekUNO <filip.dudzik.uno@gmail.com>
2025-01-01 20:56:38 +0100

    Basic StylesNode for CSS

---
+++ app/src/main/java/com/kanapka_ai/web_dom_in_android/StylesNode.kt
@@ -0,0 +1,26 @@
+package com.kanapka_ai.web_dom_in_android
+
+class StylesNode {
+    private var stylesMap: MutableMap<String, String> = mutableMapOf()
+    private var childrenMap: MutableMap<String, StylesNode> = mutableMapOf()
+
+    fun setStyle(key: String, value: String) {
+        stylesMap[key] = value
+    }
+
+    fun getStyle(key: String): String? {
+        return stylesMap[key]
+    }
+
+    fun setChild(key: String, value: StylesNode) {
+        childrenMap[key] = value
+    }
+
+    fun getChild(key: String): StylesNode? {
+        return childrenMap[key]
+    }
+
+    fun getChildren(): MutableMap<String, StylesNode> {
+        return childrenMap
+    }
+}
