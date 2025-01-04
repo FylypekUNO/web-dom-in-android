2ca34d0e5f2fa195c5d21b82883e788a3bdb3d3f
FylypekUNO <filip.dudzik.uno@gmail.com>
2025-01-05 00:31:13 +0100

    ComputeStyles template

---
+++ app/src/main/java/com/kanapka_ai/web_dom_in_android/dom/ComputeStyles.kt
@@ -0,0 +1,18 @@
+package com.kanapka_ai.web_dom_in_android.dom
+
+fun computeDirectStyles(component: Component, stylesRoot: StylesNode): Map<String, String> {
+    return mapOf()
+}
+
+fun computeAllStyles(component: Component, stylesRoot: StylesNode): Map<String, String> {
+    return mapOf()
+}
+
+fun computeAllStyles(
+    component: Component,
+    stylesRoot: StylesNode,
+    directStyles: Map<String, String>
+): Map<String, String> {
+    return mapOf()
+}
+
--- app/src/main/java/com/kanapka_ai/web_dom_in_android/dom/Page.kt
+++ app/src/main/java/com/kanapka_ai/web_dom_in_android/dom/Page.kt
@@ -1,8 +1,19 @@
 package com.kanapka_ai.web_dom_in_android.dom
 
+import android.content.Context
+import android.view.View
+
 class Page {
     public var body: Component = Component()
 
     private var stylesRoot: StylesNode = StylesNode()
+
+    fun render(): RenderNode {
+        return RenderNode()
+    }
+
+    fun createNativeView(context: Context): View {
+        return View(context)
+    }
 
 }
